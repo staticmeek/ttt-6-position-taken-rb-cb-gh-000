c93cf75ae6b0a389524ce2485d1a028b1eb800e0
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-25 04:39:04 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,7 @@
 # code your #position_taken? method here!
-def position_taken?
+def position_taken? (board, index)
+  board = [" "," "," "," "," "," "," "," "," "]
+  index = 0
+  if board[index] == " " || board[index] == "" || board[index] == nil
+  end
 end
