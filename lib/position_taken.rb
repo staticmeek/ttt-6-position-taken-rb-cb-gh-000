6df5ed9e87f5c7ae26e7b129d86465b22165ba2f
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-25 05:25:45 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,8 @@
 # code your #position_taken? method here!
-board = [" "," "," "," "," "," "," "," "," "]
-index = 0
 def position_taken? (board, index)
+  board = [" "," "," "," "," "," "," "," "," "]
+  index = 0
+
   if board[index] == "" || board[index] " " || board[index] == nil
     false
   else
