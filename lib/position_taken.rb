ed0423e3528648e38f98b3d2f024cd2faf572085
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-25 04:47:22 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken? (board, index)
   index = 0
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-  else board[index] == "X" || board[index] == "O"
+  else board[index] == "X" || board[index] == "O" || board[index] == "x" || board[index] == "o"
     return true
   end
 end
