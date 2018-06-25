08b454208a54b486c4772dd3996d750d9a178252
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-25 04:32:41 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?
-  if array[index] == " "
+  if ((array[index] == " ") || (array[index] == ""))
+  else
+    "The spot is Taken"
 
 end
