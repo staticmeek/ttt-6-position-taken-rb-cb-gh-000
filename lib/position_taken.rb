ab19c9f0fb324ea86d950d64ed5b86593cb70959
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-25 04:58:45 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,9 @@
 def position_taken? (board, index)
   board = ["","","","","","","","",""]
   index = 0
-  if board[index] == nil || board[index] == "" || board[index] == " "
-        return false
-      else board[index] == "X" || board[index] == "O"
+  if board[index] == "X" || board[index] == "O"
         return true
+      else board[index] == nil || board[index] == "" || board[index] == " " 
+        return false
   end
 end
